9c6ec9f968fe014776aa5e226b49d25334dfc111
dreamscached <dreamscache.d@gmail.com>
2022-04-12 16:23:07 +0200

    bind to versions; use cache key

--- .github/scripts/fetch-renpy-sdk.sh
+++ .github/scripts/fetch-renpy-sdk.sh
@@ -3,7 +3,7 @@ if [ -d build/renpy ]; then exit 0; fi
 
 mkdir -p build
 
-wget -q --show-progress https://www.renpy.org/dl/6.99.12.4/renpy-6.99.12.4-sdk.tar.bz2 -O build/renpy-6.99.12.4-sdk.tar.bz2
-(cd build && tar xf renpy-6.99.12.4-sdk.tar.bz2)
-rm build/renpy-6.99.12.4-sdk.tar.bz2
-mv build/renpy-6.99.12.4-sdk build/renpy
+wget -q --show-progress "https://www.renpy.org/dl/$RENPY_RELEASE/renpy-$RENPY_RELEASE-sdk.tar.bz2" -O "build/renpy-$RENPY_RELEASE-sdk.tar.bz2"
+(cd build && tar xf "renpy-$RENPY_RELEASE-sdk.tar.bz2")
+rm "build/renpy-$RENPY_RELEASE-sdk.tar.bz2"
+mv "build/renpy-$RENPY_RELEASE-sdk" build/renpy
