212945bce8bccb3a43e08e8d28ebcb670394cd5e
dreamscached <dreamscache.d@gmail.com>
2022-05-20 15:15:56 +0200

    Lint spritepacks

--- .github/scripts/compile-submod.sh
+++ .github/scripts/compile-submod.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
-mkdir -p "build/ddlc/game/mshMod"
-find -not \( -path "./build" -prune \) -iname "*.rpy" -exec cp --parents \{\} "build/ddlc/game/mshMod" \;
-build/renpy/renpy.sh "build/ddlc" compile 2>&1 | tee build/compile.log | perl -ne 'print if (/^game\/mshMod[^\s]*: \w*Warning/ || !/^game\//)' | sed 's/game\/mshMod\///g'
+
+# Clear MAS logs
+find build/ddlc/log -type f -iname '*.log' -delete
+
+# Copy .rpy files from mod folder
+mkdir -p build/ddlc/game/mshMod
+find mod -iname '*.rpy' -exec cp --parents \{\} build/ddlc/game/mshMod \;
+
+# Move spritepacks into their respective locations
+find spritepacks -mindepth 2 -maxdepth 2 -type d -exec cp -RT \{\} build/ddlc/ \;
+
+# Run build and join build output and spj.log together
+build/renpy/renpy.sh build/ddlc compile 2>&1 \
+    | tee build/compile.log \
+    | perl -ne 'print if (/^game\/mshMod[^\s]*: \w*Warning/ || !/^game\//)' \
+    | sed 's/game\/mshMod\///g'
+perl -ne 'print if (/^.*!ERROR! T_T.*$/)' build/ddlc/log/spj.log \
+    | tee -a build/compile.log
+
 if grep -Eq '^.*Error:.*$|^File ".*", line .*:.*$' build/compile.log; then exit 1; fi
-find "build/ddlc/game/mshMod" -type f -not -iname "*.rpyc" -delete
-mkdir -p "build/out/game/Submods"
-mv "build/ddlc/game/mshMod" "build/out/game/Submods/$(perl -ne 'printf $1 if /name="([^"]*)"/' "mod/00_header.rpy")"
+if tail -n +9 build/ddlc/log/spj.log | grep -Eq '^.*!ERROR! T_T.*$'; then exit 1; fi
+
+# Move compiled files to build/out
+find build/ddlc/game/mshMod -type f -not -iname '*.rpyc' -delete
+mkdir -p build/out/game/Submods
+mv build/ddlc/game/mshMod "build/out/game/Submods/$(perl -ne 'printf $1 if /name="([^"]*)"/' "mod/00_header.rpy")"
