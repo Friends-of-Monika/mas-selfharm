667ff4bf0dac7a1ad39d84d30b4f4a1ee9da5271
dreamscached <dreamscache.d@gmail.com>
2022-05-20 21:34:36 +0200

    Add spritepack packaging hook

---
+++ .github/scripts/compile-spritepacks.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+tmp="$(mktemp -d)"
+find spritepacks -mindepth 2 -maxdepth 2 -type d -exec sh -c 'cp -r "$0" "$1/$(basename "$0")"' \{\} "$tmp" \;
+old_pwd="$PWD"
+cd "$tmp"
+zip -9 -r "spritepacks-$VERSION.zip" .
+cd "$PWD"
+mv "$tmp/spritepacks-$VERSION.zip" build/out
+rm -rf "$tmp"
